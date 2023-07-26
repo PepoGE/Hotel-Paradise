03063fc023b22fbb28eb927c77d314fa7014f44d
José Luis Garza Espinoza <peporinxd@gmail.com>
2023-07-25 21:27:33 -0600

    Añadida el menu de Busqueda (HTML, CSS)

--- hotelparadise/hotelparadise/src/main/java/com/josegarza/hotelparadise/controller/HuespedController.java
+++ hotelparadise/hotelparadise/src/main/java/com/josegarza/hotelparadise/controller/HuespedController.java
@@ -1,8 +1,14 @@
 package com.josegarza.hotelparadise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HuespedController {
 
+	@GetMapping("/huespedes")
+	public String goReservas() {
+		return "menu-huespedes";
+	}
+	
 }
--- hotelparadise/hotelparadise/src/main/java/com/josegarza/hotelparadise/controller/ReservaController.java
+++ hotelparadise/hotelparadise/src/main/java/com/josegarza/hotelparadise/controller/ReservaController.java
@@ -1,8 +1,14 @@
 package com.josegarza.hotelparadise.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReservaController {
 
+	@GetMapping("/reservas")
+	public String goReservas() {
+		return "menu-reservas";
+	}
+	
 }
