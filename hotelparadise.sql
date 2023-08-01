014fadd3567f46b38a803f9d6ac18ae6b6c96e0f
José Luis Garza Espinoza <peporinxd@gmail.com>
2023-07-31 19:38:01 -0600

    Creado los triggers para añadir automaticamente el valor de la reservacion (SQL)

--- hotelparadise.sql
+++ hotelparadise.sql
@@ -9,6 +9,27 @@ valor decimal(10,2),
 formaPago varchar(50)
 );
 
+
+-- Creamos los nuevos triggers para calcular el valor automáticamente
+DELIMITER //
+
+-- Trigger para el evento INSERT
+CREATE TRIGGER calcular_valor_reserva_insert BEFORE INSERT ON Reservas
+FOR EACH ROW
+BEGIN
+  SET NEW.valor = 100 * DATEDIFF(NEW.fecha_salida, NEW.fecha_entrada);
+END;
+
+-- Trigger para el evento UPDATE
+CREATE TRIGGER calcular_valor_reserva_update BEFORE UPDATE ON Reservas
+FOR EACH ROW
+BEGIN
+  SET NEW.valor = 100 * DATEDIFF(NEW.fecha_salida, NEW.fecha_entrada);
+END;
+//
+DELIMITER ;
+
+
 CREATE TABLE Huespedes(
 id_huespeded int auto_increment not null primary key,
 nombre varchar(100) not null,
@@ -19,6 +40,11 @@ telefono varchar(10) not null,
 id_reserva int NULL,
 FOREIGN KEY (id_reserva) REFERENCES Reservas(id_reserva) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+INSERT INTO Reservas (fecha_entrada, fecha_salida, formaPago)
+VALUES ('2023-08-05', '2023-08-08', 'Tarjeta de Débito'),
+		('2024-02-02', '2024-02-11', 'Tarjeta de Crédito'),
+        ('2023-04-26', '2023-05-01', 'Pago en Efectivo');
 
 Select * from Reservas;
 Select * from Huespedes;
