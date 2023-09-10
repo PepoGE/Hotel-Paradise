774ff7a14ab7704e007c6be0ed85ee5ce0d38b41
José Luis Garza Espinoza <peporinxd@gmail.com>
2023-09-10 14:40:51 -0600

    Añadida la funcion de convertir los registros de huespedes y reservas en PDF, Excel y copiarlo en el Portapapeles. Se Añadió la libreria de DataTables así como la libreria de JQuery. Queda pendiente poner en el nombre del archivo la fecha que se realizo la copia para mejorar los registros

---
+++ hotelparadise/hotelparadise/src/main/resources/static/JS/convertir-archivos-reservas.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const table = $('#table-reservas').DataTable({
+        dom: '',
+        buttons: [
+            {
+                extend: 'copy',
+                exportOptions: {
+                    columns: ':visible'
+                },
+                title: 'Registros de Reservas Hotel Paradise'
+            },
+            {
+                extend: 'excel',
+                exportOptions: {
+                    columns: ':visible'
+                },
+                title: 'Registros de Reservas Hotel Paradise'
+            },
+            {
+                extend: 'pdf',
+                exportOptions: {
+                    columns: ':visible'
+                },
+                title: 'Registros de Reservas Hotel Paradise'
+            }
+        ],
+        language: {
+            buttons: {
+                copyTitle: '', //Aqui se muestra el mensaje de sucess
+                copyKeys: 'Presiona <i>ctrl</i> o <i>\u2318</i> + <i>C</i> para copiar los datos de la tabla al portapapeles. <br><br>Para cancelar, haz clic en este mensaje o presiona Esc.',
+                copySuccess: {
+                    _: '', //Aqui el numero de cosas copiadas
+                    1: '' //En caso de ser sola una fila copiada
+                }
+            }
+        }
+    });
+
+    $('.export-button').on('click', function() {
+        // Encuentra el índice del botón clickeado
+        const index = $('.export-button').index(this);
+
+        // Triggers de los botones correspondientes
+        table.button(index).trigger();
+    });
+});
